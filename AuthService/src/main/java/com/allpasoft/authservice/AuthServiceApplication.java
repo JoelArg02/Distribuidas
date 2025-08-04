8d05bc0abe9ac36bc1a51aa7bbbf66e609421aa6
Joel Daniel Arguello Espinosa <joel.darguello@gmail.com>
2025-08-04 01:11:47 -0500

    Eliminar la anotación @EnableFeignClients en AuthServiceApplication

--- AuthService/src/main/java/com/allpasoft/authservice/AuthServiceApplication.java
+++ AuthService/src/main/java/com/allpasoft/authservice/AuthServiceApplication.java
@@ -2,14 +2,12 @@ package com.allpasoft.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EnableFeignClients
 @ComponentScan("com.allpasoft.authservice")
 public class AuthServiceApplication {
 
