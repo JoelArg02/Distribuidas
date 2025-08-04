7df685a592fd12c6b5ca324cdec64e434fe53c19
Joel Daniel Arguello Espinosa <joel.darguello@gmail.com>
2025-08-04 13:47:42 -0500

    Eliminar el controlador AutorController y su lógica asociada

--- ms-publish/src/main/java/publicaciones/controller/AutorController.java
+++ ms-publish/src/main/java/publicaciones/controller/AutorController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@RequestMapping("/autores")
+@RequestMapping("/")
 public class AutorController {
 	@Autowired
 	private AutorService autorService;
